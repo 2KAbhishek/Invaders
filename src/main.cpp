e21dc84c44c4ef8a910e4cca90c024333c2d3b5e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-15 13:08:46 +0530

    Add glfw error callback

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,8 @@
+#include <cstdio>
+#include <GLFW/glfw3.h>
 
+// Error callback required for GLFW
+void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error : %s\n", description);
+}
