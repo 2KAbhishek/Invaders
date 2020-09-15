91c1f9317273dbaf5397e03005960eb3e966ee5b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-15 13:09:29 +0530

    Initialize glfw

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,16 @@ void error_callback(int error, const char *description)
 {
     fprintf(stderr, "Error : %s\n", description);
 }
+
+int main(int argc, char *argv[])
+{
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    return 0;
+}
