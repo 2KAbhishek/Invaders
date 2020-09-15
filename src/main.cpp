9197debc45b7b2211565548962dd06e0600c9883
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-15 13:10:32 +0530

    Add glfw hints for using OpenGL 3.3 or higher

--- src/main.cpp
+++ src/main.cpp
@@ -17,5 +17,11 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Hints to make GLFW use OpenGL context version 3.3 or higher
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     return 0;
 }
