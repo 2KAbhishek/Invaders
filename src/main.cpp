df1e815800a64e88cafefd0adab99c7799866e62
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-01 01:14:49 +0530

    Add gl_debug method

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,35 @@
     case glerror:              \
         snprintf(error, sizeof(error), "%s", #glerror)
 
+//  Debug OpenGL
+inline void gl_debug(const char *file, int line)
+{
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR)
+    {
+        char error[128];
+
+        switch (err)
+        {
+            GL_ERROR_CASE(GL_INVALID_ENUM);
+            break;
+            GL_ERROR_CASE(GL_INVALID_VALUE);
+            break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION);
+            break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION);
+            break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY);
+            break;
+        default:
+            snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR");
+            break;
+        }
+
+        fprintf(stderr, "%s - %s: %d\n", error, file, line);
+    }
+}
+
 // Buffer
 struct Buffer
 {
